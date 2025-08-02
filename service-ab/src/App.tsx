c5272cf79711331dd36ada38ed489e1b864a7aa4
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-02 18:21:19 +0530

    chore: exportable component in servvice-ab (remote-service)

--- service-ab/src/App.tsx
+++ service-ab/src/App.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Counter from './Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Service AB</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Counter />
     </>
   )
 }
